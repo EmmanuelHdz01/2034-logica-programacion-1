f73f13f3ca31e1345ae3322dd1bb01e3a6033e66
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-01-22 12:59:39 -0600

    Comentarios y mejora de texto al pedir numero

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+// Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt('Agrega un numero por favor');
+let numeroUsuario = prompt('Agrega un numero entre 1 y 10 por favor');
 
 
 console.log(numeroUsuario);
-
+/*
+Este codigo realiza 
+la comparacion
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert('El numero es el numero secreto');
+    // Acertamos, fue verdadera la condicion
+    alert(`Acertaste, tu numero fue: ${numeroUsuario}`);
 } else {
+    // No fue verdadera la condicion
     alert('Ups, este no es el numero secreto, intenta nuevamente');
 }
