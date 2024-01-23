43db242034f866a4a8a956d3470b2cee1e275d11
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-01-22 23:04:57 -0600

    Condicion de intentos y break

--- app.js
+++ app.js
@@ -2,30 +2,32 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+//let palabraVeces = "vez";
+let maximosIntentos = 3;
 while (numeroUsuario != numeroSecreto) {
-    
-    numeroUsuario = prompt('Agrega un numero entre 1 y 10 por favor');
+    numeroUsuario = prompt("Agrega un numero entre 1 y 10 por favor");
 
     console.log(numeroUsuario);
     /*
-    Este codigo realiza 
-    la comparacion
-    */
+        Este codigo realiza 
+        la comparacion
+        */
     if (numeroUsuario == numeroSecreto) {
         // Acertamos, fue verdadera la condicion
-        alert(`Acertaste, tu numero fue: ${numeroUsuario} Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, tu numero fue: ${numeroUsuario} Lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces"}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
-            alert('El numero secreto es menor');
+            alert("El numero secreto es menor");
         } else {
-            alert('El numero secreto es mayor');
+            alert("El numero secreto es mayor");
         }
-        intentos ++;
-        palabraVeces = 'veces';
+        intentos++;
+        //palabraVeces = "veces";
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
+            break;
+        }
         // No fue verdadera la condicion
         // alert('Ups, este no es el numero secreto, intenta nuevamente');
-        
     }
-    
 }
