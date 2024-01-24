38bf638fabec121ccc0f416fb39f7d016545b3a3
Emmanuel Hernández <43358285+EmmanuelHdz01@users.noreply.github.com>
2024-01-23 23:17:44 -0600

    Actualizacion de ingreso de rangos

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // Variables
-let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+let numeroSecreto = 0;
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraVeces = "vez";
 let maximosIntentos = 3;
+let rangoDeNumeros = 0;
 
-// console.log(numeroSecreto);
+rangoDeNumeros = parseInt(prompt('Ingresa hasta que numero seria el maximo para adivinar'));
+
+numeroSecreto = Math.floor(Math.random() * rangoDeNumeros) + 1;
+
+console.log(numeroSecreto);
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt("Agrega un numero entre 1 y 10 por favor"));
+    numeroUsuario = parseInt(prompt(`Agrega un numero entre 1 y ${rangoDeNumeros} por favor`));
 
     console.log(typeof(numeroUsuario));
     /*
